64c678d8dd596263708dc5abef738840d35af218
p3dr0g4br13lj3r0n1m0 <161092653+p3dr0g4br13lj3r0n1m0@users.noreply.github.com>
2024-05-17 21:52:34 -0300

    Create pasteldevento.js
    criação do arquivo js verifica se o numero é par ou impar

---
+++ pasteldevento.js
@@ -0,0 +1,9 @@
+let numeros = [2,4,6,8,10]
+
+for(let i = 0; i < numeros.length; i++){
+  if(numero[i] % 2 ===0){
+    console.log(numeros[1] + "é um numero par);
+                ) else{
+ console.log(numeros[1] + "é um numero ímpar)
+             }
+    
